1e2ecc8328ca9040ba7f2b1b45190a74e9ce2f77
nadkad <nadya.kadayifci@stonybrook.edu>
2016-09-10 19:25:23 -0400

    Etc

---
+++ MobileApp/app/src/main/java/com/bignerdranch/android/hackhealth/Daily_Log.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.hackhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Daily_Log extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily__log);
+    }
+}
